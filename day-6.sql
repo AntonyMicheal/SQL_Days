bbe3edc451b08555335f3f375b085336e7ea7995
Antony Micheal <starkantony356@gmail.com>
2022-07-28 00:55:35 +0530

    Updated day - 6

--- day-6.sql
+++ day-6.sql
@@ -6,3 +6,11 @@ select * from products;
 
 select * from products where price = 3.0 or coffee_origin = "euthiopia";
 
+select * from products where price < 3.0;
+
+
+use coffee_shop;
+select * from customers where phone_number is null;
+
+select * from customers where phone_number is not null;
+
