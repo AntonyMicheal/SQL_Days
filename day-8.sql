91a171c3d6badcbeb54230b0dd5eb686ba03c28f
Antony Micheal <starkantony356@gmail.com>
2022-07-30 17:48:17 +0530

    updating day-8

--- day-8.sql
+++ day-8.sql
@@ -11,3 +11,25 @@ select * from products order by price desc;
 select first_name, last_name from customers where first_name like '_a%' order by first_name asc;
 
 
+-- How to use distinct --
+select coffee_origin from products;
+
+select distinct coffee_origin from products;
+
+select distinct customer_id from orders where order_time between '2017-02-01' and '2017-02-28';
+
+select distinct customer_id, product_id from orders where order_time between '2017-02-01' and '2017-02-28';
+
+--  How to use limits --
+
+select * from customers limit 5;
+
+select * from customers limit 5 offset 7; -- displays 5 rows after id 7
+
+select * from customers  order by first_name asc limit 5;
+
+-- How to use column name aliases -- 
+
+select product_name as coffee, price, coffee_origin as country from products;
+
+
